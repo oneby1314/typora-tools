4d305525baf318b9a753596fd0753fe054d0cb45
oneby1314 <891734032@qq.com>
2021-02-05 17:34:39 +0800

    解决 java -jar 执行程序笔记乱码问题

--- typora-tools.bat
+++ typora-tools.bat
@@ -1,3 +1,3 @@
 @echo off
-if exist %1\ (echo dir not permitted) else (java -jar C:\Users\Heygo\Desktop\Codes\typora-tools\typora-tools.jar %1)
+if exist %1\ (echo dir not permitted) else (java -Dfile.encoding=utf-8 -jar C:\Users\Heygo\Desktop\Codes\typora-tools\typora-tools.jar %1)
 pause
